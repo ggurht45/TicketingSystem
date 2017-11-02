9f5d3831a585fa0ff50453854117f3009fabf52e
Jahangir Iqbal <iqbal.jahangir45@gmail.com>
2017-11-02 10:33:08 -0400

    saving work

--- src/main/java/demos/SyncDemo.java
+++ src/main/java/demos/SyncDemo.java
@@ -3,34 +3,25 @@ package demos;
 class Counter {
     static int count = 20000;
 
-//    public static void decrement() {
-//        count--;//count = count-1;
-//    }
-
+    //note synchronized
     public synchronized static void decrement() {
         count--;//count = count-1;
-    }
-
-}
-
-class TunaCustomer implements Runnable {
-
-    public void run() {
-        try {
-            for (int i = 10000; i > 0; i--) {
-                Counter.decrement();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
 
 public class SyncDemo {
 
+    //this will be executed inside the run method.
+    public static void decrementCounter() {
+        for (int i = 10000; i > 0; i--) {
+            Counter.decrement();
+        }
+    }
+
     public static void main(String[] args) {
-        Thread t1 = new Thread(new TunaCustomer(), "bobby");
-        Thread t2 = new Thread(new TunaCustomer(), "joe");
+        //create threads; with lambda functions.
+        Thread t1 = new Thread(() -> SyncDemo.decrementCounter(), "bobby");
+        Thread t2 = new Thread(() -> SyncDemo.decrementCounter(), "joe");
 
         //start them
         t1.start();
@@ -47,11 +38,4 @@ public class SyncDemo {
         //show count
         System.out.println("final count: " + Counter.count);
     }
-
 }
-
-
-//lambda ex.
-//        Thread t2 = new Thread(() -> {
-//            System.out.println("hello;");
-//        }, "bobby");
